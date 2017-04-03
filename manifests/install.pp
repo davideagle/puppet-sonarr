cd9738afb19baab64641e409c510dbb3da2cb5c6
Mike MacLeod <mikemacleod@gmail.com>
2017-04-02 21:01:46 -0400

    Setup NzbDrone repo

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,15 @@
 class sonarr::install inherits sonarr {
 
+  include ::apt
+  apt::source { 'sonarr':
+    location => 'http://apt.sonarr.tv/',
+    repos    => 'main',
+    key      => {
+      'id'     => 'FDA5DFFC',
+      'server' => 'keyserver.ubuntu.com',
+    },
+  }
+
   package { ['nzbdrone']:
     ensure => $sonarr::package_ensure,
   }
